689c6b30aeef3920ee2f119bed5965d2171cf330
Lucky-Jain-cmd <lucky.jain_cs23@gla.ac.in>
2025-06-27 18:15:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+       int n = arr.length+1;
+       int xor1=0,xor2=0;
+       for(int i=0;i<n-1;i++){
+            xor1 = xor1^arr[i];
+       }
+       for(int i=1;i<=n;i++){
+            xor2 = xor2^i;
+       }
+       return xor1^xor2;
+       
+    }
+    
+}
