eba9cfd8399c660110908a8d706d2efd532348b2
Lucky-Jain-cmd <lucky.jain_cs23@gla.ac.in>
2025-07-08 12:59:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
@@ -1,19 +1,21 @@
-// User function Template for Java
-class Solution {
-    public static void fizzBuzz(int number) {
-        // Write your code here.
-        if(number%3==0 && number%5==0){
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a%3==0 && a%5==0){
             System.out.println("FizzBuzz");
         }
-        else if(number%3==0){
+        else if(a%3==0){
             System.out.println("Fizz");
         }
-        else if(number % 5==0){
+        else if(a%5==0){
             System.out.println("Buzz");
         }
         else{
-            System.out.println(number);
-        }
-        
+            System.out.println(a);
+        }   
     }
 }
