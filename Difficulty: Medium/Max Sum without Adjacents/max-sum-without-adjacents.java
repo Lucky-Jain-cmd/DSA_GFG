a1469ef8cb1af69d054616b440bf4f4aba7002a9
Lucky Jain <lucky.jain_cs23@gla.ac.in>
2025-09-23 12:08:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+class Solution {
+    int findMaxSum(int arr[]) {
+        // code here
+        int n=arr.length;
+        int m[] = new int[n];
+        m[0]=arr[0];
+        m[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            m[i]=Math.max(arr[i]+m[i-2],m[i-1]);
+            
+        }
+        return m[n-1];
+    }
+}
